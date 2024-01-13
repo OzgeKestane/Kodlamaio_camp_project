f6252e12a778d5c5cf4d427c95e0e051ab08903e
ozgek <ozgek@OZGEKESTANE>
2024-01-13 17:01:48 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
+{ //bir classın default berişim belirteci internal'dır. sadece o katman erişebilir. 
+    //public: diğer katmanların erişimine açık
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
